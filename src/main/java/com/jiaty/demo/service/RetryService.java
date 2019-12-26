d16dda3d432fd9bccaf28682c679994af33ef985
贾腾宇 <tengyu.jia@jihuiduo.cn>
2019-12-26 09:56:35 +0800

    feat: StringUtils and retry

---
+++ src/main/java/com/jiaty/demo/service/RetryService.java
@@ -0,0 +1,16 @@
+package com.jiaty.demo.service;
+
+/**
+ * @program: practice
+ * @description: Spring的重试机制
+ * @author: jiaty
+ * @create: 2019-12-25 15:52
+ **/
+
+public class RetryService {
+
+
+
+
+
+}

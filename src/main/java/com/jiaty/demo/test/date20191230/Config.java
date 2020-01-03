241e58da015e499c9ec9e03fbc64e660b0bf5e76
贾腾宇 <tengyu.jia@jihuiduo.cn>
2020-01-03 17:41:13 +0800

    feat: @value

---
+++ src/main/java/com/jiaty/demo/test/date20191230/Config.java
@@ -0,0 +1,22 @@
+package com.jiaty.demo.test.date20191230;
+
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+/**
+ * @program: practice
+ * @description: 动态配置
+ * @author: jiaty
+ * @create: 2019-12-30 17:50
+ **/
+
+@Component
+@Data
+public class Config {
+
+    @Value("${failedretryTime}")
+    public String failedretryTime;
+
+
+}

19a43dd832069f765b445eb82d1efe58b211739d
hengdii <hengdi6266@163.com>
2020-02-11 21:03:58 +0800

    feat:keySet and BigDecimal

--- src/main/java/com/jiaty/demo/test/date20191127/CollectionStreamFifterTest.java
+++ src/main/java/com/jiaty/demo/test/date20191127/CollectionStreamFifterTest.java
@@ -2,7 +2,6 @@ package com.jiaty.demo.test.date20191127;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 /**
  * @program: Practice
@@ -16,25 +15,23 @@ public class CollectionStreamFifterTest {
 
     public static void main(String[] args) {
         List<Person> personList = new ArrayList<>();
-        Person person0 = new Person("123","男",22,1);
-        Person person1 = new Person("123","男",23,1);
-        Person person2 = new Person("123","男",23,1);
-        Person person3 = new Person("123","男",6);
+        Person person0 = new Person("123", "男", 22, 1);
+        Person person1 = new Person("123", "男", 23, 1);
+        Person person2 = new Person("123", "男", 23, 1);
+        Person person3 = new Person("123", "男", 23, 6);
         personList.add(person0);
         personList.add(person1);
         personList.add(person2);
         personList.add(person3);
 
-        System.out.println(personList);
+//        System.out.println(personList);
 
-        List<Person> collect = personList.stream().filter(result -> result.getStatus() == 1)
-                .filter(result -> result.getAge() != 23)
-                .collect(Collectors.toList());
+        personList.stream().filter(result -> result.getAge() == 22 || result.getAge() == 23).forEach(result -> {
+//            System.out.println(1111);
+            System.out.println(result.getAge());
+        });
 
-        System.out.println(collect);
     }
-
-
 
 
 }

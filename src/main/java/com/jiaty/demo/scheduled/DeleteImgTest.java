7f3b54e3fefde4241376b3dc3c3652011baf9db8
贾腾宇 <tengyu.jia@jihuiduo.cn>
2020-03-03 17:10:38 +0800

    feat:scheduled

---
+++ src/main/java/com/jiaty/demo/scheduled/DeleteImgTest.java
@@ -0,0 +1,39 @@
+package com.jiaty.demo.scheduled;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @program: Practice
+ * @description: 定时任务
+ * @author: Jiaty
+ * @create: 2020-03-03 16:28
+ **/
+@Component
+public class DeleteImgTest implements ApplicationRunner {
+
+
+//    @Resource
+//    ScheduledExecutorService scheduledExecutorService;
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        System.out.println(args);
+        System.out.println("这个是测试ApplicationRunner接口");
+        startDeleteImgService();
+    }
+
+    //定时器
+    public void startDeleteImgService() {
+        System.out.println("启动删除＝＝＝＝");
+//        scheduledExecutorService.schedule(RunTest::new, Long.parseLong("3"), TimeUnit.SECONDS);
+    }
+
+}

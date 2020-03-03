b5ab700a558cc40a16a11ff24a019d61d78c8b1e
hengdii <hengdi6266@163.com>
2020-03-03 17:10:38 +0800

    feat:scheduled

---
+++ src/main/java/com/jiaty/demo/scheduled/RunTest.java
@@ -0,0 +1,26 @@
+package com.jiaty.demo.scheduled;
+
+/**
+ * @program: Practice
+ * @description:
+ * @author: Jiaty
+ * @create: 2020-03-03 16:36
+ **/
+
+public class RunTest implements Runnable {
+    /**
+     * When an object implementing interface <code>Runnable</code> is used
+     * to create a thread, starting the thread causes the object's
+     * <code>run</code> method to be called in that separately executing
+     * thread.
+     * <p>
+     * The general contract of the method <code>run</code> is that it may
+     * take any action whatsoever.
+     *
+     * @see Thread#run()
+     */
+    @Override
+    public void run() {
+        System.out.println("123123");
+    }
+}

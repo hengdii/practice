f957235a93281f68b15995f6bef6a16aa6da70cb
hengdii <hengdi6266@163.com>
2020-06-02 18:59:40 +0800

    mapper scan

--- src/main/java/com/jiaty/demo/PracticeApplication.java
+++ src/main/java/com/jiaty/demo/PracticeApplication.java
@@ -1,9 +1,11 @@
 package com.jiaty.demo;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.jiaty.demo.mybatis.dao")
 public class PracticeApplication {
 
     public static void main(String[] args) {

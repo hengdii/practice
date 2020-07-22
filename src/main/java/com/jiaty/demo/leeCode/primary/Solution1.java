11a90327de4a64708661e75d3911b232e9523b6e
hengdii <hengdi6266@163.com>
2020-07-22 11:32:03 +0800

    a little test

--- src/main/java/com/jiaty/demo/leeCode/primary/Solution1.java
+++ src/main/java/com/jiaty/demo/leeCode/primary/Solution1.java
@@ -29,30 +29,89 @@ public class Solution1 {
 
 
     public static int reverse(int x) {
-        int resultNum = 0;
-        long longx = Long.parseLong(String.valueOf(x));
+        long resultNum;
         StringBuilder sb = new StringBuilder();
-        if (x == 0 || x != longx) {
-            return resultNum;
+        if (x == 0 || x >= Integer.MAX_VALUE || x <= Integer.MIN_VALUE) {
+            return 0;
         }
         if (x > 0) {
             char[] x1 = String.valueOf(x).toCharArray();
             for (int i = x1.length - 1; i >= 0; i--) {
                 sb.append(x1[i]);
             }
-            resultNum = Integer.valueOf(sb.toString());
+            resultNum = Long.parseLong(sb.toString());
+            if (resultNum >= Integer.MAX_VALUE) {
+                resultNum = 0L;
+            }
         } else {
             char[] x2 = String.valueOf(x).toCharArray();
             for (int i = x2.length - 1; i >= 1; i--) {
                 sb.append(x2[i]);
             }
-            int i = Integer.parseInt(sb.toString());
-            resultNum = -i;
+            resultNum = Long.parseLong(sb.toString());
+            resultNum = -resultNum;
+            if (resultNum <= Integer.MIN_VALUE) {
+                resultNum = 0L;
+            }
+
         }
-        return resultNum;
+        return (int) resultNum;
     }
 
     public static void main(String[] args) {
-        System.out.println(reverse(1534236469));
+        System.out.println(reverse2(-123));
+        System.out.println(reverse3(-123));
+    }
+
+    /**
+     * 使用long类型和int(x)==x 来判断是否
+     *
+     * @param x
+     * @return
+     */
+    public static int reverse1(int x) {
+        long n = 0;
+        while (x != 0) {
+            n = n * 10 + x % 10;
+            x = x / 10;
+        }
+        return (int) n == n ? (int) n : 0;
+    }
+
+    /**
+     * 官方解法
+     *
+     * @param x
+     * @return
+     */
+    public static int reverse2(int x) {
+        int rev = 0;
+        while (x != 0) {
+            int pop = x % 10;
+            x /= 10;
+            if (rev > Integer.MAX_VALUE / 10 || (rev == Integer.MAX_VALUE / 10 && pop > 7)) {
+                return 0;
+            }
+            if (rev < Integer.MIN_VALUE / 10 || (rev == Integer.MIN_VALUE / 10 && pop < -8)) {
+                return 0;
+            }
+            rev = rev * 10 + pop;
+        }
+        return rev;
+    }
+
+    /**
+     * 利用StringBuilder 的 reverse
+     * @param x
+     * @return
+     */
+    public static int reverse3(int x){
+        StringBuilder reverse = new StringBuilder(String.valueOf(x)).reverse();
+        int flag = 1;
+        if (x<0){
+            flag = -1;
+        }
+
+        return 0;
     }
 }

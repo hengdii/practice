403b7c0467d11b711e3dbe2d89ce4d63c0631a68
hengdii <hengdi6266@163.com>
2020-07-27 15:19:12 +0800

    392

--- src/main/java/com/jiaty/demo/leeCode/primary/Solution1025.java
+++ src/main/java/com/jiaty/demo/leeCode/primary/Solution1025.java
@@ -12,10 +12,10 @@ public class Solution1025 {
     /**
      * 爱丽丝和鲍勃一起玩游戏，他们轮流行动。爱丽丝先手开局。
      * 
-     * 最初，黑板上有一个数字 N 。在每个玩家的回合，玩家需要执行以下操作：
+     * 最初，黑板上有一个数字 n 。在每个玩家的回合，玩家需要执行以下操作：
      * 
-     * 选出任一 x，满足 0 < x < N 且 N % x == 0 。
-     * 用 N - x 替换黑板上的数字 N 。
+     * 选出任一 x，满足 0 < x < n 且 n % x == 0 。
+     * 用 n - x 替换黑板上的数字 n 。
      * 如果玩家无法执行这些操作，就会输掉游戏。
      * 
      * 只有在爱丽丝在游戏中取得胜利时才返回 True，否则返回 false。假设两个玩家都以最佳状态参与游戏。
@@ -36,18 +36,18 @@ public class Solution1025 {
      * 
      * 提示：
      * 
-     * 1 <= N <= 1000
+     * 1 <= n <= 1000
      */
 
-    public static boolean divisorGame(int N) {
+    public static boolean divisorGame(int n) {
         boolean result = true;
         int i = 1;
         while (result) {
-            int key = getResult(N);
-            if (N == key) {
+            int key = getResult(n);
+            if (n == key) {
                 result = false;
             }
-            N = key;
+            n = key;
             i++;
         }
         if (i % 2 == 0) {

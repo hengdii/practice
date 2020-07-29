a72813b339aa2a3d36baf24377240953586cdb56
hengdii <hengdi6266@163.com>
2020-07-29 18:51:10 +0800

    27，28

---
+++ src/main/java/com/jiaty/demo/leecode/primary/Solution28.java
@@ -0,0 +1,37 @@
+package com.jiaty.demo.leecode.primary;
+
+/**
+ * @program: Practice
+ * @description: 实现strStr() java 中的indexOf()
+ * @author: Jiaty
+ * @create: 2020-07-29 17:33
+ **/
+
+public class Solution28 {
+
+    public static int strStr(String haystack, String needle) {
+        if (haystack == null){
+            return -1;
+        }
+        if (needle == null || needle.length() == 0) {
+            return 0;
+        }
+        for (int i = 0; i < haystack.length(); i++) {
+
+
+
+
+        }
+
+
+
+    }
+
+
+    public static void main(String[] args) {
+
+
+    }
+
+
+}

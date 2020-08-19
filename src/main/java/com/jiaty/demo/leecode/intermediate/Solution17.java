0383e6cb05c002b2968dfe7f8826d9ec707f286a
hengdi <hengdi6266@163.com>
2020-08-19 15:46:43 +0800

    17官方题解

--- src/main/java/com/jiaty/demo/leecode/intermediate/Solution17.java
+++ src/main/java/com/jiaty/demo/leecode/intermediate/Solution17.java
@@ -14,7 +14,25 @@ import java.util.Map;
 
 public class Solution17 {
 
-    Map<String, String> phone = new HashMap<String, String>() {{
+    /**
+     * 回溯算法：
+     *  回溯算法是一种通过琼剧所有可能情况来找到所有解的算法。如果一个候选解最后被发现不是可行解，回溯算法会舍弃他，
+     *  并且在前面的一些步骤做出一些修改，并重新尝试找到可行解
+     *
+     *  官方题解说这种做法是回溯，但是我看就是纯粹的递归....
+     *
+     *  官方：
+     *     给出如下回溯函数BackTrack(Combination,next_digits)，
+     *     它将一个目前已经产生的组合combination和接下来准备输入的数字next_digits作为参数
+     *
+     *     如果没有更多的数字需要被输入，那意味着当前的组合已经产生好了
+     *     如果还有数字需要被输入
+     *     遍历下一个数字所对应的所有映射的字母
+     *     将当前的字母添加到组合最后，也就是combination = combination + letter
+     *     重复这个过程，输入剩下的数字
+     */
+
+    static Map<String, String> phone = new HashMap<String, String>() {{
         put("2", "abc");
         put("3", "def");
         put("4", "ghi");
@@ -25,9 +43,18 @@ public class Solution17 {
         put("9", "wxyz");
     }};
 
-    List<String> output = new ArrayList<String>();
+    static List<String> output = new ArrayList<String>();
 
-    public void backtrack(String combination, String next_digits) {
+    /**
+     * 官方题解：
+     *  时间复杂度：O(3的N次方  *  4的M次方)，N：输入数字中对应3个字母的数目。M：输入数字中对应4个字母的数目。 N+M是输入数字的总数
+     *
+     *  空间复杂度 O(3的N次方  *  4的M次方)，这是因为需要保存这么多个结果
+     *
+     * @param combination
+     * @param next_digits
+     */
+    public static void backtrack(String combination, String next_digits) {
         // if there is no more digits to check
         if (next_digits.length() == 0) {
             // the combination is done
@@ -48,11 +75,18 @@ public class Solution17 {
         }
     }
 
-    public List<String> letterCombinations(String digits) {
+    public static List<String> letterCombinations(String digits) {
         if (digits.length() != 0) {
             backtrack("", digits);
         }
         return output;
+    }
+
+    public static void main(String[] args) {
+        String digits = "24";
+        System.out.println(letterCombinations(digits));
+
+
     }
 
 }

197ff868871cb517c289cd58edf78f403e394c80
hengdi <hengdi6266@163.com>
2020-08-21 09:53:22 +0800

    二分查找

---
+++ src/main/java/com/jiaty/demo/leecode/primary/Solution.java
@@ -0,0 +1,94 @@
+package com.jiaty.demo.leecode.primary;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+/**
+ * @program: practice
+ * @description:
+ * @author: Jiaty
+ * @create: 2020-08-19 16:17
+ **/
+
+public class Solution {
+    /**
+     * 大神们, 求一道算法啊
+     * 已知一个List<String>
+     * list.add("123");
+     * list.add("1");
+     * list.add("123456");
+     * list.add("123");
+     * list.add("124");
+     * 从list中求出一个集合, 里面的元素彼此没有包含关系(去掉被包含的)
+     * 比如上面的元素123包含1, 那么取123; 123456包含123, 取123456
+     * 最后要的集合肯定只有两个元素, 就是123456和124
+     * <p>
+     * 还有（前缀树
+     */
+
+
+    public static List<String> getNotRepeatingList(List<String> list) {
+        if (list.size() == 0 || list.size() == 1) {
+            return list;
+        }
+        List<String> stringList = new ArrayList<>();
+        //这一步是因为假如说有重复的，他就不保留
+        list = new CopyOnWriteArrayList<>(list);
+        for (int i = 0; i < list.size(); i++) {
+            int j = list.size() - 1;
+            boolean flg = false;
+            while (j > -1) {
+                if (j == i || j == i + 1) {
+                    j--;
+                    continue;
+                }
+                if (list.get(j).contains(list.get(i))) {
+                    flg = true;
+                    break;
+                }
+                j--;
+            }
+            if (!flg) {
+                stringList.add(list.get(i));
+                list.remove(i);
+            }
+        }
+        return stringList;
+    }
+
+
+    public static List<String> getNotRepeat(List<String> list) {
+        Iterator<String> ite = list.iterator();
+        while (ite.hasNext()) {
+            String s = ite.next();
+            for (int i = 0; i < list.size(); i++) {
+                if (list.indexOf(s) == i) {
+                    continue;
+                }
+                if (list.get(i).contains(s)) {
+                    ite.remove();
+                    break;
+                }
+            }
+        }
+        return new ArrayList<>();
+    }
+
+
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        list.add("123");
+        list.add("1");
+        list.add("12234567");
+        list.add("123");
+        list.add("12345678");
+        list.add("12345678");
+        list.add("1246");
+        System.out.println(getNotRepeatingList(list));
+//        System.out.println(getNotRepeat(list));
+    }
+
+
+}
---
+++ src/main/java/com/jiaty/demo/leecode/primary/Solution704.java
@@ -0,0 +1,109 @@
+package com.jiaty.demo.leecode.primary;
+
+/**
+ * @program: practice
+ * @description: 二分查找
+ * @author: Jiaty
+ * @create: 2020-08-20 10:50
+ **/
+
+public class Solution704 {
+    /**
+     * 二分查找  模版一
+     * <p>
+     * 初始条件：left = 0, right = length-1
+     * 终止：left > right
+     * 向左查找：right = mid-1
+     * 向右查找：left = mid+1
+     *
+     * @param nums
+     * @param target
+     * @return
+     */
+    public static int search(int[] nums, int target) {
+        int result = -1;
+        if (nums.length == 0) {
+            return result;
+        }
+        int start = 0;
+        int end = nums.length - 1;
+        while (start <= end) {
+            int mid = (start + end) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] > target) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+//        int[] nums = new int[]{-1, 0, 3, 5, 9, 12};
+//
+//        int target = 2;
+//
+//        System.out.println(search(nums, target));
+//
+//        int x = 8000;
+//        System.out.println(mySqrt(x));
+
+        int n = 3;
+        System.out.println(guessNumber1(n));
+        System.out.println(guessNumber(n));
+    }
+
+
+    public static int mySqrt(int x) {
+        int start = 0;
+        int end = x;
+
+        return -1;
+    }
+
+
+    public static int guessNumber(int n) {
+        if (guess(n) == 0) {
+            return n;
+        }
+        int i = 1;
+        int end = n - 1;
+        while (i <= n) {
+            int mid = i + (end - i) / 2;
+            if (guess(mid) == 0) {
+                return mid;
+            } else if (guess(mid) > 0) {
+                i = mid - 1;
+            } else {
+                end = mid + 1;
+            }
+        }
+        return -1;
+    }
+
+
+    public static int guessNumber1(int n) {
+        int low = 1;
+        int high = n;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (guess(mid) == 0) {
+                return mid;
+            } else if (guess(mid) > 0) {
+                low = mid - 1;
+            } else {
+                high = mid + 1;
+            }
+        }
+        return -1;
+    }
+
+
+    private static int guess(int n) {
+        return n == 1 ? 0 : n > 1 ? -1 : 1;
+    }
+
+
+}

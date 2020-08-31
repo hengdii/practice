a2fd2b501b708ea267f619ce9bb04df7980de2f8
hengdi <hengdi6266@163.com>
2020-08-31 13:49:18 +0800

    557 and BubbleSort

---
+++ src/main/java/com/jiaty/demo/arithmetic/BubbleSort.java
@@ -0,0 +1,48 @@
+package com.jiaty.demo.arithmetic;
+
+import java.util.Arrays;
+
+/**
+ * @program: practice
+ * @description: 冒泡排序
+ * @author: Jiaty
+ * @create: 2020-08-31 10:39
+ **/
+
+public class BubbleSort {
+
+
+    // 冒泡排序，a表示数组，n表示数组大小
+    public static int[] bubbleSort(int[] a, int n) {
+        if (n <= 1) {
+            return a;
+        }
+
+        for (int i = 0; i < n; ++i) {
+            // 提前退出冒泡循环的标志位
+            boolean flag = false;
+            for (int j = 0; j < n - i - 1; ++j) {
+                if (a[j] > a[j + 1]) { // 交换
+                    int tmp = a[j];
+                    a[j] = a[j + 1];
+                    a[j + 1] = tmp;
+                    flag = true;  // 表示有数据交换
+                }
+            }
+            if (!flag) {
+                break;  // 没有数据交换，提前退出
+            }
+        }
+        return a;
+    }
+
+    public static void main(String[] args) {
+        int [] nums = new int[]{4,5,6,3,2,1};
+        System.out.println(Arrays.toString(bubbleSort(nums, nums.length)));
+
+
+
+    }
+
+
+}
--- src/main/java/com/jiaty/demo/leecode/primary/Solution557.java
+++ src/main/java/com/jiaty/demo/leecode/primary/Solution557.java
@@ -10,7 +10,7 @@ package com.jiaty.demo.leecode.primary;
 public class Solution557 {
 
     public String reverseWords(String s) {
-        if (s.length() == 0 || " ".equals(s)) {
+        if (s == null || s.length() == 0 || " ".equals(s)) {
             return s;
         }
         String[] strings = s.split(" ");
@@ -18,8 +18,7 @@ public class Solution557 {
         for (String string : strings) {
             stringBuilder.append(new StringBuilder(string).reverse().append(" "));
         }
-        String substring = stringBuilder.toString().substring(0, s.length());
-        return substring;
+        return stringBuilder.toString().substring(0, s.length());
     }
 
 //    private String rever(String s) {

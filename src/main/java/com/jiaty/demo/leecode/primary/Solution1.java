f680a2c9f814d86e31fa3c8c3433e1e7583e075d
hengdi <hengdi6266@163.com>
2020-12-30 18:58:11 +0800

    135

--- src/main/java/com/jiaty/demo/leecode/primary/Solution1.java
+++ src/main/java/com/jiaty/demo/leecode/primary/Solution1.java
@@ -85,4 +85,15 @@ public class Solution1 {
 //        System.out.println(twoSum4(nums,target));
     }
 
+    public int[] twoSum2(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i])) {
+                return new int[]{i, map.get(target - nums[i])};
+            }
+            map.put(nums[i], i);
+        }
+        return null;
+    }
+
 }

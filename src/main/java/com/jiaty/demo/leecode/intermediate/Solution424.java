517e25dd67642087802347ab76dcc66e095277fd
hengdi <hengdi6266@163.com>
2021-02-02 18:08:52 +0800

    424

---
+++ src/main/java/com/jiaty/demo/leecode/intermediate/Solution424.java
@@ -0,0 +1,29 @@
+package com.jiaty.demo.leecode.intermediate;
+
+/**
+ * @program: practice
+ * @description:
+ * @author: Jiaty
+ * @create: 2021-02-02 18:08
+ **/
+
+public class Solution424 {
+
+    public int characterReplacement(String s, int k) {
+        int[] num = new int[26];
+        int n = s.length();
+        int maxn = 0;
+        int left = 0, right = 0;
+        while (right < n) {
+            num[s.charAt(right) - 'A']++;
+            maxn = Math.max(maxn, num[s.charAt(right) - 'A']);
+            if (right - left + 1 - maxn > k) {
+                num[s.charAt(left) - 'A']--;
+                left++;
+            }
+            right++;
+        }
+        return right - left;
+    }
+
+}
